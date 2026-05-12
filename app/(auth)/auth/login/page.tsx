13e8b6d90a3e81b7ebf68e4373775a6cc57340ac
Michael Salam <michaeldimimu@gmail.com>
2026-05-12 03:53:52 +0100

    feat: create pin entry page ui

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const LoginPage = () => {
   return (
-    <div>
+    <>
       <h1 className="font-semibold text-2xl mb-1">Welcome back</h1>
       <p className="mb-6">Enter your Cave phone number to continue</p>
 
@@ -11,11 +11,11 @@ const LoginPage = () => {
 
       <div className="flex items-center text-sm justify-center gap-1 mt-2">
         <p>Don&apos;t have a Cave Bank account?</p>
-        <Link href="/auth/signup" className="font-semibold">
+        <Link href="/auth/signup" className="font-semibold text-foreground">
           Sign up
         </Link>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ components/custom/AuthPinEntry.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+import { Eye, Lock } from "lucide-react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
+const AuthPinEntry = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  return (
+    <form>
+      <div className="relative">
+        <Lock
+          className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-500"
+          size={18}
+        />
+        <input
+          type={passwordVisible ? "text" : "password"}
+          className="w-full text-xl border border-gray-400 rounded-lg pl-12 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:border-transparent"
+          placeholder="••••"
+        />
+        <Button
+          onClick={() => setPasswordVisible((prev) => !prev)}
+          type="button"
+          variant="link"
+          className="absolute right-3 top-1/2 -translate-y-1/2!"
+          size="icon-lg"
+        >
+          <Eye className="size-5" />
+        </Button>
+      </div>
+
+      <div className="flex items-center justify-end">
+        <Link
+          href="/auth/reset-pin"
+          className="text-sm font-medium mt-2 text-foreground"
+        >
+          Forgot PIN?
+        </Link>
+      </div>
+    </form>
+  );
+};
+
+export default AuthPinEntry;
